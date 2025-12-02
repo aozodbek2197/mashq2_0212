a18809eaf1c66d1c02fc2c02377194db5b552a6c
aozodbek2197 <aozodbek2197@gmail.com>
2025-12-02 15:41:08 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+# # 6-mashq
+# string = lambda text: len(text)
+
+# print(string("Salom"))
+# print(string("Yaxshimisiz"))
+# print(string("Tom"))
+# # 7-mashq
+# musbat_manfiy = lambda x: "Musbat" if x > 0 else ("manfiy" if x < 0 else "nol")
+
+# print(musbat_manfiy(7))
+# print(musbat_manfiy(-8))
+# print(musbat_manfiy(0))
+# # 8-mashq
+# ucburchak_yuzasi = lambda asos, balandlik: 0.5 * asos * balandlik
+
+# print(ucburchak_yuzasi(10, 8))
+# print(ucburchak_yuzasi(5, 12))
+# print(ucburchak_yuzasi(7, 6))
+# # 9-mashq
+# juft = lambda x: x % 2 == 0
+
+# print(juft(8))
+# print(juft(-3))
+# print(juft(0))
+# # 10-mashq
+# katta_kichik = lambda x: "Katta" if x > 10 else "kichik"
+
+# print(katta_kichik(11))
+# print(katta_kichik(17))
+# print(katta_kichik(3))
